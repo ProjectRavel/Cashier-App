80ce2ca2a87bc62e269ce3f1bb7c8f5830dec9ea
ProjectRavel <rafaelcodeid@gmail.com>
2024-08-08 01:31:39 +0700

    Instalattion Vite & db

---
+++ KasirApp-React/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import NavbarComponents from "./NavbarComponents";
+
+function App() {
+
+  return (
+    <>
+      <NavbarComponents />
+    </>
+  );
+}
+
+export default App;
