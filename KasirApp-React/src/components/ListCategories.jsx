8973aa006b54df5da06386e6a1525333234d59bd
ProjectRavel <rafaelcodeid@gmail.com>
2024-08-10 01:35:28 +0700

    Change Direction Folder & Categories feature

--- KasirApp-React/src/components/ListCategories.jsx
+++ KasirApp-React/src/components/ListCategories.jsx
@@ -1,12 +1,27 @@
 import { useEffect, useState } from "react";
-import { Col } from "react-bootstrap";
+import { Col, ListGroup } from "react-bootstrap";
 import { API_URL } from "../utils/constans";
 import axios from "axios";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faUtensilSpoon,
+  faCoffee,
+  faCheese,
+} from "@fortawesome/free-solid-svg-icons";
 
+const Icon = ({ nama }) => {
+  if (nama === "Makanan")
+    return <FontAwesomeIcon icon={faUtensilSpoon} className="mx-2" />;
+  if (nama === "Minuman")
+    return <FontAwesomeIcon icon={faCoffee} className="mx-2" />;
+  if (nama === "Cemilan")
+    return <FontAwesomeIcon icon={faCheese} className="mx-2" />;
+
+  return <FontAwesomeIcon icon={faUtensilSpoon} className="mx-2" />;
+};
 
 function ListCategories() {
-
-  const [categories, setCategories] = useState([])
+  const [categories, setCategories] = useState([]);
 
   useEffect(() => {
     axios
@@ -20,13 +35,22 @@ function ListCategories() {
       });
   }, []);
 
-  console.log(categories);
-
   return (
     <Col md={2} mt="2">
-      <h4>
+      <h4 className="mb-4">
         <strong>Daftar Kategori</strong>
       </h4>
+      <ListGroup>
+        {categories &&
+          categories.map((category) => (
+            <ListGroup.Item key={category.id}>
+              <h5>
+                <Icon nama={category.nama} />
+                {category.nama}
+              </h5>
+            </ListGroup.Item>
+          ))}
+      </ListGroup>
     </Col>
   );
 }
