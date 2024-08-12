38ddd9c1e2e230de7182203e1f229b929e0e2a8c
ProjectRavel <rafaelcodeid@gmail.com>
2024-08-13 00:26:31 +0700

    ADd List for Keranjang

--- KasirApp-React/src/components/Hasil.jsx
+++ KasirApp-React/src/components/Hasil.jsx
@@ -1,12 +1,25 @@
-import { Col } from 'react-bootstrap'
+import { Col, ListGroup, Row } from "react-bootstrap";
 
-function Hasil() {
+function Hasil({ keranjangs }) {
   return (
     <Col md={2} mt="2">
-        <h4><strong>Hasil</strong></h4>
-         
+      <h4>
+        <strong>Hasil</strong>
+      </h4>
+      <ListGroup variant="flush">
+        {keranjangs.length !== 0 &&
+          keranjangs.map((keranjang) => (
+            <ListGroup.Item key={keranjang.id}>
+              <Row>
+                <Col md={4}>{keranjang.product.nama}</Col>
+                <Col md={4}>{keranjang.jumlah}</Col>
+                <Col md={4}>Rp. {keranjang.totalHarga}</Col>
+              </Row>
+            </ListGroup.Item>
+          ))}
+      </ListGroup>
     </Col>
-  )
+  );
 }
 
-export default Hasil
+export default Hasil;
