25361c34b2d652867e8a1f24788bd7460b77a1dd
ProjectRavel <rafaelcodeid@gmail.com>
2024-08-13 00:25:25 +0700

    Add UseEffect For Keranjang Update

--- KasirApp-React/src/App.jsx
+++ KasirApp-React/src/App.jsx
@@ -5,7 +5,7 @@ import { useEffect, useState } from "react";
 import { API_URL } from "./utils/constans";
 import axios from "axios";
 import Menus from "./components/Menus";
-import Swal from 'sweetalert2'
+import Swal from "sweetalert2";
 
 function App() {
   const [menus, setMenus] = useState([]);
@@ -18,22 +18,31 @@ function App() {
       .then((response) => {
         const menus = response.data;
         setMenus(menus);
-        console.log("test");
       })
       .catch((error) => {
         console.error("Error fetching data: ", error);
       });
   }, []);
 
+  useEffect(() => {
+    axios
+      .get(API_URL + "keranjangs")
+      .then((response) => {
+        const keranjang = response.data;
+        setKeranjangs(keranjang); //
+      })
+      .catch((error) => {
+        console.error("Error fetching data: ", error);
+      });
+  }, [keranjangs]);
+
   const filterMenusByCategory = (category) => {
     setCategoriesSelected(category);
-    // Filter menus by category using the API_URL and the selected category name.
     axios
       .get(API_URL + "products?category.nama=" + category)
       .then((response) => {
         const menus = response.data;
         setMenus(menus);
-        console.log("test");
       })
       .catch((error) => {
         console.error("Error fetching data: ", error);
@@ -41,26 +50,53 @@ function App() {
   };
 
   const masukkanKeKeranjang = (value) => {
-
-    const keranjang = {
-      jumlah: 1,
-      total_harga: value.harga,
-      product: value
-    }
-
     axios
-    .post(API_URL + "keranjangs", keranjang)
-    .then((response) => {
-      Swal.fire({
-        title: "Sukses Masuk Keranjang!",
-        text: `${keranjang.product.nama} Berhasil Ditambahkan!`,
-        icon: "success"
-      });
-    })
-    .catch((error) => {
-      console.error("Error fetching data: ", error);
-    });
+      .get(API_URL + "keranjangs?product.id=" + value.id)
+      .then((response) => {
+        if (response.data.length == 0) {
+          const keranjang = {
+            jumlah: 1,
+            total_harga: value.harga,
+            product: value,
+          };
 
+          axios
+            .post(API_URL + "keranjangs", keranjang)
+            .then((response) => {
+              Swal.fire({
+                title: "Sukses Masuk Keranjang!",
+                text: `${keranjang.product.nama} Berhasil Ditambahkan!`,
+                icon: "success",
+              });
+            })
+            .catch((error) => {
+              console.error("Error fetching data: ", error);
+            });
+        } else {
+          const keranjang = {
+            jumlah: response.data[0].jumlah + 1,
+            total_harga: response.data[0].total_harga + value.harga,
+            product: value,
+          };
+
+          axios
+            .put(API_URL + "keranjangs/" + response.data[0].id, keranjang)
+            .then((response) => {
+              Swal.fire({
+                title: "Sukses Masuk Keranjang!",
+                text: `${keranjang.product.nama} Berhasil Ditambahkan!`,
+                icon: "success",
+              });
+            })
+            .catch((error) => {
+              console.error("Error fetching data: ", error);
+            });
+        }
+      })
+      .catch((error) => {
+        console.error("Error fetching data: ", error);
+      });
+    1;
   };
 
   return (
@@ -90,7 +126,7 @@ function App() {
                   ))}
               </Row>
             </Col>
-            <Hasil />
+            <Hasil keranjangs={keranjangs} />
           </Row>
         </Container>
       </div>
