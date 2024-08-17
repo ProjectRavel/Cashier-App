8374736988fd14a15c3b00d85dcecaf72a141679
ProjectRavel <rafaelcodeid@gmail.com>
2024-08-18 01:44:37 +0700

    Add Async Function

--- KasirApp-React/src/components/TotalBayar.jsx
+++ KasirApp-React/src/components/TotalBayar.jsx
@@ -7,75 +7,69 @@ import axios from "axios";
 import { API_URL } from "../utils/constans";
 
 function TotalBayar({ totalBayar, keranjangs }) {
-  const submitTotalBayar = (totalBayarPembayaran, keranjangsItem) => {
-    Swal.fire({
-      title: "Konfirmasi Pembayaran",
-      text: "Apakah anda yakin ingin melakukan pembayaran?",
-      icon: "question",
-      showCancelButton: true,
-      confirmButtonColor: "#3085d6",
-      cancelButtonColor: "#d33",
-      confirmButtonText: "Ya",
-    }).then((result) => {
-      if (result.isConfirmed) {
-        axios.get(API_URL + "pesanans").then((response) => {
-          for (var i = 0; i < response.data.length; i++) {
-            axios
-              .delete(API_URL + "pesanans/" + response.data[i].id)
-              .then((res) => {
-                console.log(res);
-              })
-              .catch((err) => {
-                Swal.fire({
-                  title: "Gagal Melakukan Pembayaran!",
-                  text: "Terdapat kesalahan saat melakukan pembayaran. Silahkan coba lagi.",
-                  icon: "error",
-                  confirmButtonText: "Coba Lagi",
-                });
-                console.log(err);
-              });
-          }
+  const submitTotalBayar = async (totalBayarPembayaran, keranjangsItem) => {
+  Swal.fire({
+    title: "Konfirmasi Pembayaran",
+    text: "Apakah anda yakin ingin melakukan pembayaran?",
+    icon: "question",
+    showCancelButton: true,
+    confirmButtonColor: "#3085d6",
+    cancelButtonColor: "#d33",
+    confirmButtonText: "Ya",
+  }).then(async (result) => {
+    if (result.isConfirmed) {
+      try {
+        // Tampilkan loading
+        Swal.fire({
+          title: "Memproses Pembayaran...",
+          text: "Mohon tunggu beberapa saat.",
+          icon: "info",
+          allowOutsideClick: false,
+          showConfirmButton: false,
         });
+
+        // Dapatkan pesanan
+        const response = await axios.get(API_URL + "pesanans");
+
+        // Hapus setiap pesanan
+        for (let i = 0; i < response.data.length; i++) {
+          await axios.delete(API_URL + "pesanans/" + response.data[i].id);
+        }
+
+        // Buat pesanan baru
         const pesanan = {
           total_pembayaran: totalBayarPembayaran,
           menus: keranjangsItem,
         };
 
-        axios
-          .post(API_URL + "pesanans", pesanan)
-          .then(() => {
-            Swal.fire({
-              title: "Pembayaran Sukses!",
-              text: "Terima kasih atas pesanan anda!",
-              icon: "success",
-              confirmButtonText: "Lihat Pesanan",
-            }).then(() => {
-              window.location.href = "/sukses";
+        await axios.post(API_URL + "pesanans", pesanan);
 
-              keranjangsItem.map((keranjang) => {
-                return axios
-                  .delete(API_URL + "keranjangs/" + keranjang.id)
-                  .then((res) => {
-                    console.log(res);
-                  })
-                  .catch((err) => {
-                    console.error(err);
-                  });
-              });
-            });
-          })
-          .catch((err) => {
-            Swal.fire({
-              title: "Gagal Melakukan Pembayaran!",
-              text: "Terdapat kesalahan saat melakukan pembayaran. Silahkan coba lagi.",
-              icon: "error",
-              confirmButtonText: "Coba Lagi",
-            });
-            console.log(err)
-          });
+        // Hapus keranjang
+        for (let keranjang of keranjangsItem) {
+          await axios.delete(API_URL + "keranjangs/" + keranjang.id);
+        }
+
+        Swal.fire({
+          title: "Pembayaran Sukses!",
+          text: "Terima kasih atas pesanan anda!",
+          icon: "success",
+          confirmButtonText: "Lihat Pesanan",
+        }).then(() => {
+          window.location.href = "/sukses";
+        });
+
+      } catch (err) {
+        Swal.fire({
+          title: "Gagal Melakukan Pembayaran!",
+          text: "Terdapat kesalahan saat melakukan pembayaran. Silahkan coba lagi.",
+          icon: "error",
+          confirmButtonText: "Coba Lagi",
+        });
+        console.log(err);
       }
-    });
-  };
+    }
+  });
+};
 
   return (
     <div>
